e1b8ebbbdfc0a01a55b3eefe6a92e812372ff8d3
afachetti <allisonfachetti@gmail.com>
2020-06-09 22:46:45 +0000

    getting ready for deployment

--- app.rb
+++ app.rb
@@ -15,5 +15,79 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-# events_table = DB.from(:events)
-# rsvps_table = DB.from(:rsvps)
+classes_table = DB.from(:classes)
+rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(:id=>session[:user_id]).to_a[0]
+    puts @current_user.inspect
+    results = Geocoder.search("800 Elgin Rd, Evanston, IL 60201")
+    @lat_long = results.first.coordinates.join(",") 
+end
+
+get "/" do 
+    @classes = classes_table.all
+    puts @classes.inspect
+    view "classes"
+end 
+
+get "/classes/:id" do 
+    @users_table = users_table 
+    @class = classes_table.where(:id => params["id"]).to_a[0]
+    @rsvps = rsvps_table.where(:class_id => params["id"]).to_a
+    @count = rsvps_table.where(:class_id => params["id"], :going => 1).count
+    puts @class.inspect
+    view "class"
+end     
+
+get "/classes/:id/rsvps/new" do 
+    @class = classes_table.where(:id => params["id"]).to_a[0]
+    view "new_rsvp"
+end 
+
+post "/classes/:id/rsvps/create" do
+    rsvps_table.insert(:class_id => params["id"],
+                       :going => params["going"],
+                       :user_id => @current_user[:id],
+                       :comments => params["comments"])
+    @class = classes_table.where(:id => params["id"]).to_a[0]
+    view "create_rsvp"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do 
+    users_table.insert(:name => params["name"],
+                       :email => params["email"],
+                       :password => BCrypt::Password.create(params["password"]))
+    view "create_user"
+end 
+
+get "/logins/new" do 
+    view "new_login"
+end 
+
+post "/logins/create" do 
+    emailentered = params["email"]
+    passwordentered = params["password"]
+    user = users_table.where(:email => emailentered).to_a[0]
+    if user
+        puts user.inspect
+        if BCrypt::Password.new(user[:password])==passwordentered
+            session[:user_id] = user[:id]
+            view "create_login"
+        else 
+            view "create_login_failed"
+        end 
+    else
+    view "create_login_failed"
+    end 
+end 
+
+get "/logout" do 
+    session[:user_id] = nil 
+    view "logout"
+end 
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,40 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :classes do
   primary_key :id
   String :title
   String :description, text: true
+  String :teacher
   String :date
   String :location
 end
 DB.create_table! :rsvps do
   primary_key :id
-  foreign_key :event_id
+  foreign_key :class_id
+  foreign_key :user_id
   Boolean :going
-  String :name
-  String :email
   String :comments, text: true
 end
 
+DB.create_table! :users do
+    primary_key :id
+    String :name
+    String :email
+    String :password
+end 
+
 # Insert initial (seed) data
-events_table = DB.from(:events)
+classes_table = DB.from(:classes)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+classes_table.insert(title: "Pilates Fusion", 
+                    description: "Combine classic mat pilates with cardio bursts!",
+                    teacher: "Allison",
+                    date: "June 1",
+                    location: "Studio A")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+classes_table.insert(title: "Barre Basics", 
+                    description: "Calling all dancers. The isometric movements in this classic barre class will leave your legs burning!",
+                    teacher: "Allison",
+                    date: "July 1",
+                    location: "Studio B")
